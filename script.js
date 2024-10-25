824121256cd569f57cdc74ecb7aa41f470d8c03a
thekeylock <112468652+thekeylock@users.noreply.github.com>
2024-10-25 13:27:22 +0200

    e

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showaboutme() {
     var input = document.getElementById("aboutmeinput").value;
     if(input == "show")
     {
-       document.getElementById("aboutmetext").style.display = "block"; 
+       document.getElementById("aboutmetext").display = "block"; 
     }
     else{
         document.getElementById("aboutmetext").innerText = "Wrong!"
