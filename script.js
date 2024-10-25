33d9e4d215c4c9ccbc92e42d562c04245b2bc757
thekeylock <112468652+thekeylock@users.noreply.github.com>
2024-10-25 13:42:27 +0200

    h

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function showaboutme() {
     var input = document.getElementById("aboutmeinput").value;
+    console.log("Input value:", input);
     var aboutMeText = document.getElementById("aboutmetext");
+    console.log("aboutMeText element:", aboutMeText);
 
-    if (input === "show") {
-        aboutMeText.style.display = "block"; 
+    if (aboutMeText) { // Check if aboutMeText is found
+        if (input === "show") {
+            aboutMeText.style.display = "block"; 
+        } else {
+            aboutMeText.innerText = "Wrong!";
+            aboutMeText.style.display = "block";
+        }
     } else {
-        aboutMeText.innerText = "Wrong!";
-        aboutMeText.style.display = "block";
+        console.error("Element with ID 'aboutmetext' not found.");
     }
 }
